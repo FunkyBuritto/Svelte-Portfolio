e9474fdcdd8181d0b7596343aed89fa4cbe23fc2
FunkyBuritto <milanb043@gmail.com>
2022-06-23 18:22:55 +0200

    list and mobile update

--- src/commands/list.js
+++ src/commands/list.js
@@ -4,7 +4,7 @@ export const List =
 
    Ikvr                 <a href="https://github.com/FunkyBuritto/IKVR-Dragon" target="_blank"><u class="typeline">https://github.com/FunkyBuritto/IKVR-Dragon</u></a>
    Rogue Planets        <a href="https://github.com/Roboolet/ld49" target="_blank"><u class="typeline">https://github.com/FunkyBuritto/ld49</u></a>
-   Placeholder2         <a href="https://github.com/FunkyBuritto/Ikvr" target="_blank"><u class="typeline">https://github.com/FunkyBuritto/Placeholder2</u></a>
+   Friends in a galaxy  <a href="https://github.com/FunkyBuritto/Friends-in-a-galaxy" target="_blank"><u class="typeline">https://github.com/FunkyBuritto/Friends-in-a-galaxy</u></a>
 
 ───────────────────────────────────────────────────────────────────────
 <br>
